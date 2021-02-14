87e3ec5e12ccf7b0737da146dd2bb56a06fdcf6a
David Bushell <david@dbushell.com>
2021-02-14 09:14:03 +0000

    urls

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const identity = {
   name: 'David Bushell',
   email: 'hi@dbushell.com',
-  url: 'https://dbushell.com/'
+  url: 'https://dbushell.com/',
+  github: 'https://github.com/dbushell/',
+  twitter: 'https://twitter.com/dbushell/'
 };
 
 export default identity;
